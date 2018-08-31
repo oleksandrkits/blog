a5ed7f38f18fdd92206fc6285c3ff9e4e14b5b00
oleksandrkits <oleksandrkit@tahometer.com>
2018-08-31 11:49:23 +0300

    Integrated friendly routs for bloggs.

--- app/controllers/bloggs_controller.rb
+++ app/controllers/bloggs_controller.rb
@@ -64,7 +64,7 @@ class BloggsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blogg
-      @blogg = Blogg.find(params[:id])
+      @blogg = Blogg.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/blogg.rb
+++ app/models/blogg.rb
@@ -1,2 +1,4 @@
 class Blogg < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end

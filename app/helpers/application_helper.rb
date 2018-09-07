4582b5e2859b0a2eed3d7b7876b55e55b2acb56b
oleksandrkits <oleksandrkit@tahometer.com>
2018-09-07 14:37:25 +0300

    Integrated application to use custom gem.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,10 @@ module ApplicationHelper
       content_tag(:p, greeting, class: 'source-greeting' )
     end
   end
+
+
+  def copyright_generator
+    InterlinkViewTool::Renderer.copyright 'Kitsera', 'All rights reserved'
+  end
+
 end
